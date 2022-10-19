42c73e3aad3ebd5d7fe019c45fa5c9215dea2d81
saanglee <sangllee20@gmail.com>
2022-10-19 11:34:15 +0900

    feat: create UserList

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,27 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">APP</h1>
-
-    <complted-todo :todos="todos" />
-    <add-todo @add-todo="addTodo" />
-    <todo-list
-      :todos="todos"
-      @toggle-checkbox="toggleCheckbox"
-      @click-delete="deleteTodo"
-    />
-
+    <complted-todo />
+    <add-todo />
     <hr />
+    <todo-list />
+    <user-list />
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import TodoList from "./components/TodoList.vue";
 import AddTodo from "./components/AddTodo.vue";
 import CompltedTodo from "./components/CompletedTodo.vue";
-import TodoList from "./components/TodoList.vue";
+import UserList from "./components/UserList.vue";
+
 export default {
-  name: "App",
   components: {
     AddTodo,
     CompltedTodo,
     TodoList,
-  },
-  data() {
-    return {
-      todoText: "",
-      todos: [
-        { id: 1, text: "buy a car", checked: false },
-        { id: 2, text: "play game", checked: false },
-      ],
-    };
-  },
-  methods: {
-    addTodo(value) {
-      console.log("App - ", value);
-      this.todos.push({
-        id: Math.random(),
-        text: value,
-        checked: false,
-      });
-      this.todoText = "";
-    },
-    deleteTodo(id) {
-      console.log("id: ", id);
-      const index = this.todos.findIndex((todo) => {
-        return todo.id === id;
-      });
-      console.log("index: ", index);
-      this.todos.splice(index, 1);
-    },
-    toggleCheckbox({ id, checked }) {
-      console.log(id, checked);
-      const index = this.todos.findIndex((todo) => {
-        return todo.id === id;
-      });
-      this.todos[index].checked = checked;
-    },
+    UserList,
   },
 };
 </script>
---
+++ src/components/UserList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    User List
+    <div v-for="user in users" :key="user.id">
+      {{ user.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return { users: [] };
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users") // Promise를 리턴함
+      .then((response) => {
+        this.users = response.data;
+        console.log(response);
+      });
+    console.log("이게 reponse보다 먼저 찍힘");
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
