ab6035e61df6121b7556e4c00ca6b70e482b92f5
saanglee <sangllee20@gmail.com>
2022-10-21 14:27:00 +0900

    Feat: Todo Page

---
+++ src/views/Todo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    Todo
+    <v-btn @click.prevent="logout">Logout</v-btn>
+    <add-todo />
+    <completed-todo />
+    <todo-list />
+  </div>
+</template>
+
+<script>
+import TodoList from "../components/TodoList.vue";
+import TodoItem from "../components/TodoItem.vue";
+import AddTodo from "../components/AddTodo.vue";
+import CompletedTodo from "../components/CompletedTodo.vue";
+import { getAuth, signOut } from "firebase/auth";
+export default {
+  name: "Todo",
+  components: {
+    TodoList,
+    TodoItem,
+    AddTodo,
+    CompletedTodo,
+  },
+  methods: {
+    logout() {
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          console.log("log out");
+          this.$router.replace("/");
+        })
+        .catch((error) => console.log(error.message));
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
