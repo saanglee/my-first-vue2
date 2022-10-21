ca41c2d305ef84c8d83cbb1582dbc020a049a89c
saanglee <sangllee20@gmail.com>
2022-10-21 14:24:36 +0900

    firebase 초기 설정

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase/compat/app";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+const firebaseConfig = {
+  apiKey: "AIzaSyAqFRGJIiYHQcYnZ0LiY1ReLYOawgXyPTw",
+  authDomain: "vue-todo-list-b0751.firebaseapp.com",
+  projectId: "vue-todo-list-b0751",
+  storageBucket: "vue-todo-list-b0751.appspot.com",
+  messagingSenderId: "588178223136",
+  appId: "1:588178223136:web:6b0cad1c67085d6d710c45",
+  measurementId: "G-5E7L0J96RG",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+new Vue({ router, store, vuetify, render: (h) => h(App) }).$mount("#app");
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
