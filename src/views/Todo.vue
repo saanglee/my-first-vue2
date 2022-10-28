c2bea4653d0601031cef74de59ab8e240f13d94c
saanglee <sangllee20@gmail.com>
2022-10-28 11:44:47 +0900

    Feat: 저장, 삭제 로직 부모 컴포넌트인 Todo.vue로 이동

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,48 +1,129 @@
 <template>
   <div>
-    <v-btn @click.prevent="logout">Logout</v-btn>
-    <add-todo />
-
-    <todo-list />
+    <div class="d-flex justify-end">
+      <v-btn
+        @click.prevent="logout"
+        class="mt-2 mr-4 text-h6"
+        color="#757575"
+        dark
+        large
+        rounded
+        >Logout</v-btn
+      >
+    </div>
+    <add-todo v-on:addTodo="addTodo" />
+    <todo-list
+      v-bind:todoProps="todos"
+      v-bind:completedTodos="completedTodos"
+      v-on:deleteTodoById="deleteTodo"
+      v-on:toggleCheckBox="toggleCheckBox"
+    />
   </div>
 </template>
 
 <script>
 import TodoList from "../components/TodoList.vue";
-import TodoItem from "../components/TodoItem.vue";
 import AddTodo from "../components/AddTodo.vue";
-import CompletedTodo from "../components/CompletedTodo.vue";
+import db from "../main";
+
+import {
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { getAuth, signOut } from "firebase/auth";
+
 export default {
   name: "Todo",
   components: {
     TodoList,
-    TodoItem,
+
     AddTodo,
-    CompletedTodo,
+  },
+  data() {
+    return {
+      todos: [],
+      completedTodos: 0,
+    };
+  },
+
+  methods: {
+    async toggleCheckBox({ targetId, current }) {
+      console.log(targetId, current);
+      const targetItem = doc(db, "todos", targetId);
+      await updateDoc(targetItem, {
+        completed: !current,
+      });
+    },
+    async deleteTodo(targetId) {
+      const filteredTodos = this.todos.filter((todo) => todo.id !== targetId);
+      await deleteDoc(doc(db, "todos", targetId));
+      this.todos = filteredTodos;
+    },
+    async addTodo(newTodo) {
+      const newItem = { todo: newTodo, completed: false, isEditable: false };
+      if (newItem.todo) {
+        await addDoc(collection(db, "todos"), newItem);
+        this.todos.unshift(newItem);
+      }
+      return;
+    },
+    async getTodos() {
+      const querySnapShot = await getDocs(collection(db, "todos"));
+      querySnapShot.forEach((doc) => {
+        this.todos.push({
+          id: doc.id,
+          todo: doc.data().todo,
+          completed: doc.data().completed,
+          isEditable: false,
+        });
+      });
+      // this.getCompletedTodos();
+    },
+
+    logout() {
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          sessionStorage.removeItem("user");
+          this.$router.replace("/");
+          window.location.reload(true);
+        })
+        .catch((error) => console.log(error.message));
+    },
+    // getCompletedTodos() {
+    //   const parsedTodos = JSON.parse(JSON.stringify(this.todos));
+    //   const numberOfCompleted = parsedTodos.filter(
+    //     (todo) => todo.completed === true
+    //   ).length;
+    //   console.log(numberOfCompleted);
+    //   this.completedTodos = numberOfCompleted;
+    // },
+    // test2() {
+    //   this.getCompletedTodos();
+    // },
+  },
+  computed: {
+    numberOfCompletedTodo() {
+      return this.todos.filter((todo) => todo.completed).length;
+    },
   },
   created() {
-    // session getItem ( parser )
-    // undefined, null -> redirect
     const storedUser = sessionStorage.getItem("user");
     if (!storedUser) {
       this.$router.replace("/");
       return;
     }
   },
-  methods: {
-    logout() {
-      const auth = getAuth();
-      signOut(auth)
-        .then(() => {
-          sessionStorage.removeItem("user");
-          console.log("log out");
-          this.$router.replace("/");
-        })
-        .catch((error) => console.log(error.message));
-    },
+  mounted() {
+    this.addTodo();
+    this.getTodos();
+    // this.test();
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
