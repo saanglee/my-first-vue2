18316c3714232a7634cab85a186578c4ef9eff14
saanglee <sangllee20@gmail.com>
2022-10-28 11:38:31 +0900

    Polish

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,33 +3,34 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/recommended", "plugin:vuetify/base"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
-    "vue/html-self-closing": [
-      "error",
-      {
-        html: {
-          void: "never",
-          normal: "always",
-          component: "always",
-        },
-        svg: "always",
-        math: "always",
-      },
-    ],
-    "vuetify/no-deprecated-classes": "error",
+    // "vue/html-self-closing": [
+    //   "error",
+    //   {
+    //     html: {
+    //       void: "never",
+    //       normal: "always",
+    //       component: "always",
+    //     },
+    //     svg: "always",
+    //     math: "always",
+    //   },
+    // ],
     "vue/multi-word-component-names": 0,
-    "vuetify/no-legacy-grid": "error",
-    "vue/max-attributes-per-line": [
-      0,
-      {
-        singleline: {
-          max: 1,
-        },
-        multiline: {
-          max: 1,
-        },
-      },
-    ],
+    "vue/max-attributes-per-line": "off",
+
+    // "vue/max-attributes-per-line": [
+
+    // 0,
+    // {
+    //   singleline: {
+    //     max: 1,
+    //   },
+    //   multiline: {
+    //     max: 2,
+    //   },
+    // },
+    // ],
   },
 };
